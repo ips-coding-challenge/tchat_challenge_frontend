a7d46ddb293e9b2a29f28cd545980d70e70c295c
myself <android.programmation@laposte.net>
2020-08-11 19:54:23 +0200

    fixed url

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import feathers from "@feathersjs/client";
 
-const socket = io("https://tchat-challenge.vercel.app");
+const socket = io("https://tchat-challenge.herokuapp.com");
 const client = feathers();
 
 client.configure(feathers.socketio(socket));
