9af2234cc4d0c38682c48a1602f05ba549dbfb27
myself <android.programmation@laposte.net>
2020-08-11 19:50:10 +0200

    Change url

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import feathers from "@feathersjs/client";
 
-const socket = io(process.env.REACT_APP_SOCKET_URL);
+const socket = io("https://tchat-challenge.vercel.app");
 const client = feathers();
 
 client.configure(feathers.socketio(socket));
