3af2e3fd99bf5d082172a3e270db4f8ac5f75edc
ips-coding-challenge <coding_challenge@laposte.net>
2020-10-07 19:20:31 +0200

    Add bot with few commands (#1)

--- src/components/Chat/ConnectedUsers.js
+++ src/components/Chat/ConnectedUsers.js
@@ -1,17 +1,31 @@
-import React, { useEffect, useCallback, useContext } from "react";
-import client from "../../client";
-import { store, SET_CONNECTED_USERS } from "../../store/store";
+import React, { useEffect, useCallback, useContext } from 'react'
+import client from '../../client'
+import { store, SET_CONNECTED_USERS } from '../../store/store'
 
 const ConnectedUsers = () => {
   const {
     state: { user, connectedUsers, currentChannel },
     dispatch,
-  } = useContext(store);
+  } = useContext(store)
+
+  const createBot = () => {
+    return {
+      _id: 'thebot',
+      channelId: currentChannel._id.toString(),
+      user: {
+        avatar: 'https://www.freeiconspng.com/uploads/robot-icon-29.png',
+        email: '',
+        name: 'TheBot',
+        _id: 'bot',
+      },
+      userId: 'bot',
+    }
+  }
 
   const fetchConnectedUsers = useCallback(async () => {
     try {
       // console.log(`Channel._id`, channel._id);
-      const result = await client.service("connected-users").find({
+      const result = await client.service('connected-users').find({
         query: {
           channelId: currentChannel._id.toString(),
           userId: {
@@ -19,30 +33,31 @@ const ConnectedUsers = () => {
           },
         },
         paginate: false,
-      });
-      console.log(`Results`, result.data);
-      dispatch({ type: SET_CONNECTED_USERS, payload: result.data });
+      })
+      console.log(`Results`, result.data)
+      const connectedUsers = result.data.concat(createBot())
+      dispatch({ type: SET_CONNECTED_USERS, payload: connectedUsers })
     } catch (e) {
-      console.log(`error while fetching connected user`, e);
+      console.log(`error while fetching connected user`, e)
     }
-  }, [currentChannel]);
+  }, [currentChannel])
 
   useEffect(() => {
     if (currentChannel) {
-      fetchConnectedUsers();
-      client.service("connected-users").on("created", () => {
-        fetchConnectedUsers();
-      });
-      client.service("connected-users").on("removed", () => {
-        fetchConnectedUsers();
-      });
+      fetchConnectedUsers()
+      client.service('connected-users').on('created', () => {
+        fetchConnectedUsers()
+      })
+      client.service('connected-users').on('removed', () => {
+        fetchConnectedUsers()
+      })
     }
 
     return () => {
-      client.service("connected-users").removeListener("created");
-      client.service("connected-users").removeListener("removed");
-    };
-  }, [currentChannel]);
+      client.service('connected-users').removeListener('created')
+      client.service('connected-users').removeListener('removed')
+    }
+  }, [currentChannel])
 
   return (
     <div className="h-auto">
@@ -59,12 +74,12 @@ const ConnectedUsers = () => {
                   />
                   {item.user.name}
                 </li>
-              );
+              )
             })}
         </ul>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default ConnectedUsers;
+export default ConnectedUsers
